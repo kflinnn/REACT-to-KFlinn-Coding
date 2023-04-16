07d07a35526582a52ca60cf93977e033073f716b
kflinnn <kflinncoding@gmail.com>
2023-04-16 12:06:02 -0400

    updated header.js, added images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import profile from '/images/Profile.png'
 
 function Header () {
     return (
         <header>
-            {/* header content here */}
+            <img src={profile} alt="Profile Picture"/>
+            <nav>
+                <ul>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+            <div>
+                <a href="https://www.linkedin.com/in/kelley-flinn-1509a6167/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+                <a href="https://www.twitter.com/your-twitter-profile" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+                <a href="https://github.com/kflinnn" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+            </div>
+            <a href="#contact" className="cta-button">Contact Me</a>
         </header>
     );
 }
