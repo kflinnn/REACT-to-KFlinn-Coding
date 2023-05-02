1fd81fc3f4a48788acde1073a23adf1f1a94934f
kflinnn <kflinncoding@gmail.com>
2023-05-01 20:55:30 -0400

    replaced old project with project 3 link and photo

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,22 +6,22 @@ function Project () {
         <section id="work" class="page-section">
                     <h2>Work</h2>
                     <div class="flex-container">
-                        <a class="flex-item work-one" href="https://pure-island-77704.herokuapp.com/">
+                        <a class="flex-item work-one" href="https://foodi3-c3ntral.vercel.app//">
+                            <div class="work-text">
+                                <h3>Foodi3 C3ntral</h3>
+                                <span>A Meal Planning Tool</span>
+                            </div>
+                        </a>
+                        <a class="flex-item work-two" href="https://pure-island-77704.herokuapp.com/">
                             <div class="work-text">
                                 <h3>Foodie Central at Home</h3>
                                 <span>A Recipe Share</span>
                             </div>
                         </a>
-                        <a class="flex-item work-two" href="https://phoenix-ceri.github.io/FoodieCentral/">
+                        <a class="flex-item work-three" href="https://phoenix-ceri.github.io/FoodieCentral/">
                             <div class="work-text">
                                 <h3>Foodie Central</h3>
                                 <span>Locate Local Eateries</span>
-                            </div>
-                        </a>
-                        <a class="flex-item work-three" href="https://kflinnn.github.io/Quiz-Yo-Self/">
-                            <div class="work-text">
-                                <h3>Quiz Yo'Self</h3>
-                                <span>A Coding Quiz</span>
                             </div>
                         </a>
                         <a class="flex-item work-four" href="https://kflinnn.github.io/Schedule-It/">
