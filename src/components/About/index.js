afa489b83f1a27641d7273a120cf80f71a640053
kflinnn <kflinncoding@gmail.com>
2023-06-05 23:52:57 -0400

    fixed about page media query, completed contact page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,13 +5,13 @@ import Profile from '../../Images/Profile.png';
 export default function About() {
     return (
         <main class="bio-container">
-
-            <div class="image-wrapper">
-                <h2>My Name is Kelley</h2>
-                <img src={Profile} alt="Kelley" className="profile-picture" />
-            </div>
+<h2>My Name is Kelley</h2>
+            <div class="center-box">
+                <img src={Profile} alt="Kelley" class="profile-picture" />
+            
             <div class="bio-text-box">
                 <p>I am a highly motivated, creative, and personable Full-Stack Developer based out of Central Florida and is always eager to grow and learn. I have experience in frontend languages such as HTML, CSS, CSS frameworks including Bootstrap and Tailwind, and JavaScript as well as back-end technologies such as Node.js, Express.js, jQuery, REACT, and databases including MySQL, MongoDB, and GraphQL. In my desire to always be learning, I'm currently working on adding Python to my development toolkit so as to be as versatile a developer as possible. I love to collaborate with like-minded individuals who are also looking to further better the world around them through web development. Outside of the coding world, I am a musician, with a music degree in percussion, foodie, and fitness enthusiast. I also sing baritone in a barbershop quartet. Let's get together!</p>
+                </div>
             </div>
         </main>
     )
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import "./style.css";
 import { Link } from 'react-router-dom';
 
-function Header () {
+export default function Header () {
+    
+    // function toggleNav() {
+    //     const nav = document.getElementById('nav');
+    //     nav.classList.toggle('show');
+    // }
     return (
         <header>
             <h1>KFlinn Coding</h1>
+            {/* <div class="hamburger" onClick={toggleNav()}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div> */}
             <nav>
             <Link to="/">Home</Link>
             <Link to="/work">Work</Link>
@@ -15,5 +25,3 @@ function Header () {
         </header>
     );
 }
-
-export default Header;
